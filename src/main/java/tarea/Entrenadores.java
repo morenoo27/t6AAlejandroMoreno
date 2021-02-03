33c079621e45da64e57111d7ee6195cd9627859f
moreno27 <alejanmm2000@gmail.com>
2021-02-03 14:18:58 +0100

    Creada las clases con la geraquia

---
+++ src/main/java/tarea/Entrenadores.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea;
+
+/**
+ *
+ * @author alejandro
+ */
+public class Entrenadores extends Equipo{
+    
+    private double salario;
+    
+}
---
+++ src/main/java/tarea/Jugadores.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea;
+
+/**
+ *
+ * @author alejandro
+ */
+public class Jugadores extends Equipo{
+    
+    private int posicion;
+    private int numDorsal;
+    private double salario;
+}
